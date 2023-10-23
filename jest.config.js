74803fd454ec1c22cf2acb9379cb76fdb81d6c4f
teyma <teyma@ippon.fr>
2023-10-23 16:30:03 +0200

    Ajout coverage 100% jest

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,16 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/main/webapp/**/*.ts'],
+  coverageDirectory: '<rootDir>/src/test/javascript/coverage/',
+  coverageReporters: ['lcov'],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
 };
